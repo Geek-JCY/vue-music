4cc03401cc443c0f92390efedf0de28805e8b3cf
jcy <523764061@qq.com>
2018-05-23 01:28:04 +0800

    路由配置 + tab顶部组件开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import Recommend from 'components/recommend/recommend.vue'
+import Singer from 'components/singer/singer.vue'
+import Rank from 'components/rank/rank.vue'
+import Search from 'components/search/search.vue'
 
 Vue.use(Router)
 
@@ -8,8 +11,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: 'recommend'
+    },
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      name: 'singer',
+      component: Singer
+
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
     }
   ]
 })
