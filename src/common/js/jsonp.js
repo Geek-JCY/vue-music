89f9a10a1b8c60dc26ba924e4f5164332a84f5c8
jcy <523764061@qq.com>
2018-05-25 17:20:42 +0800

    jsonp应用 + Promise封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,30 @@
+import originJSONP from 'jsonp'
+
+/**
+ * @description 封装成熟悉使用的jsonp方法
+ * @param {string} url
+ * @param {obj} data
+ * @param {obj} option
+ * @returns {promise obj} new Promise
+ */
+export default function jsonp(url, data, option) {
+  url = (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      !err ? resolve(data) : reject(err)
+    })
+  })
+}
+
+/**
+ * @description 传递的参数对象
+ * @param {obj} data
+ */
+function param(data) {
+  let url = ''
+  for (let key in data) {
+    let value = data[key] !== undefined ? data[key] : ''
+    url += `&${key}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
+}
