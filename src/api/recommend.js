3150e176cfa2060220ca59e22bf7e8cb0ab33318
jcy <523764061@qq.com>
2018-05-29 23:09:55 +0800

    歌单数据接口分析

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,14 +1,31 @@
-
 import jsonp from 'common/js/jsonp'
-import {commonParams, options} from './config'
+import {commonParams, options} from 'api/config'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+  const data = Object.assign({}, commonParams, {
+    uin: 0,
+    platform: 'h5',
+    needNewCode: 1
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function getDiscList() {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
 
   const data = Object.assign({}, commonParams, {
-    platform: 'h5',
-    uin: 0,
-    needNewCode: 1
+    picmid: 1,
+    platform: 'yqq',
+    loginUin: 0,
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random()
   })
 
   return jsonp(url, data, options)
